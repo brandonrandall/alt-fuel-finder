48053ce005e4d4cbd406b8f0e493fa2a849f64e5
Brandon Randall <br.mhcexchange@gmail.com>
2017-08-30 10:28:31 -0600

    adds api key

--- app/services/nrel_services.rb
+++ app/services/nrel_services.rb
@@ -1,11 +1,17 @@
 class NRELServices
 
   def initialize
-    @connection = Faraday.get("https://developer.nrel.gov/")
-    binding.pry
+    @connection = Faraday.get("https://developer.nrel.gov")
   end
 
-  def stations
-    
+  def api_key
+    "HDsU4wOvZAKTe5iPzQjijeLkF30ORbFiI4ScBEl5"
+  end
+
+  def receive_all_stations
+    response = @connection.get("/api/alt-fuel-stations/v1.format?api_key=#{api_key}")
+    JSON.parse(response.body)
+    binding.pry
+  end
 
 end
