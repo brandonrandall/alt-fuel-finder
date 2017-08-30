c368cc33a18eec4b97692e18a265a30a9721db39
Brandon Randall <br.mhcexchange@gmail.com>
2017-08-30 09:45:24 -0600

    changed location of spec to features

--- spec/features/user_can_search_for_alt_fuel_stations_spec.rb
+++ spec/features/user_can_search_for_alt_fuel_stations_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 describe "User Search" do
   scenario "user can search for alt fuel stations by zip code" do
-    brandon = User.create
-    visit "/"
+
+    brandon = User.create(name: "Brandon")
+
+    visit '/'
     fill_in "80203"
     click_on "Locate"
     expect(page).to be("/search")
     expect(page).to have_content("top 10 closest stations; propane and electric")
+    expect(page).to have_css(".station", count: 10)
     expect(page).to have_content("Name:")
     expect(page).to have_content("Address:")
     expect(page).to have_content("Fuel Types:")
